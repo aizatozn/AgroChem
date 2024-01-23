1b20ba61d14fd5462e22c2c08c3204fb536fbe6d
Aizat <aizatozn@gmail.com>
2024-01-23 21:41:33 +0600

    Initial Commit

---
+++ AgroLab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AgroLab
+//
+//  Created by Aizat Ozbekova on 23/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
