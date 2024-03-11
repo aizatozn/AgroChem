c2165159bf07ff5999b3c8cb577ed99fea88b794
Aizat <aizatozn@gmail.com>
2024-03-11 14:44:52 +0600

    some changes

--- AgroChem/Application/AppDelegate.swift
+++ AgroChem/Application/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?)
     -> Bool {
-
+        mainWindow.overrideUserInterfaceStyle = .light
         services.forEach {
             _ = $0.application?(application, didFinishLaunchingWithOptions: launchOptions)
         }
