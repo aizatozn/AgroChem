986d8c7651fc623f6a9f5b9cf637ebc42f1d873b
Aizat <aizatozn@gmail.com>
2024-04-08 22:38:47 +0600

    some changes

---
+++ AgroChem/Scenes/Client/ClientMedicine/ClientMedicinePresentable.swift
@@ -0,0 +1,8 @@
+//
+//  ClientMedicinePresentable.swift
+//  AgroChem
+//
+//  Created by Aizat Ozbekova on 8/4/24.
+//
+
+import Foundation
---
+++ AgroChem/Scenes/Client/ClientMedicine/ClientMedicineViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ClientMedicineViewModel.swift
+//  AgroChem
+//
+//  Created by Aizat Ozbekova on 8/4/24.
+//
+
+import Foundation
